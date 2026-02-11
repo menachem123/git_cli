9695e263fa5a2e1f7e3c652d91704ddf405d431e
menachem amitai <mendiamitai770@gmail.com>
2026-02-11 09:35:25 -0500

    add packet_large

--- f1.py
+++ f1.py
@@ -1,29 +1,37 @@
 
 
 def load_csv_function (csv):
-    External_list = []
+    external_ip = []
     with open (csv,"r",encoding="utf-8") as file :
         for line in file:
-            External_list.append(list(line.strip().split(",")))
+            external_ip.append(list(line.strip().split(",")))
+
+    return external_ip
 
 
-    return External_list
-
-
-def external_ip(all_ip):
+def external_ip(source_ip):
     list_external_ip = []
-    for ip in all_ip:
+    for ip in source_ip:
         if not ip[1].startswith("192.168") and not ip[1].startswith("10"):
             list_external_ip.append(ip)
 
     return list_external_ip
 
-def sensitive_port (port):
+def sensitive_port (protoco):
     list_sensitive_port = []
-    for ip in port:
+    for ip in protoco:
         if not ip[4] == "SSH" and not ip[4] == "Telnet" and not ip[4] == "RDP" :
             list_sensitive_port.append(ip)
 
     return list_sensitive_port
+
+
+def packet_large (size):
+    list_packet_large = []
+    for ip in size:
+        if ip[5] > "5000" :
+            list_packet_large.append(ip)
+
+    return list_packet_large
 
 
